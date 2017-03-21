465e1d6efd118e367a7fbbcc4f5cc4f71514cf16
Dave Love <bungeemonkee@gmail.com>
2017-03-21 14:44:08 -0400

    First version: 1.0.1-beta1

---
+++ Configgy.Microsoft.Extensions.Configuration.Tests/Source/ConfigurationRootSourceTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Configgy.Microsoft.Extensions.Configuration.Source;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Configgy.Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace Configgy.Microsoft.Extensions.Configuration.Tests.Source
+{
+    [TestClass]
+    public class ConfigurationRootSourceTests
+    {
+        [TestMethod]
+        public void Constructor_Works()
+        {
+            var source = new ConfigurationRootSource();
+        }
+
+        [TestMethod]
+        public void Constructor_Takes_IConfigurationRoot()
+        {
+            var rootMock = new Mock<IConfigurationRoot>();
+
+            var source = new ConfigurationRootSource(rootMock.Object);
+
+            Assert.AreSame(rootMock.Object, source.ConfigurationRoot);
+        }
+
+        [TestMethod]
+        public void Get_Returns_False_When_Value_Is_Null()
+        {
+            const string valueName = "value";
+
+            var rootMock = new Mock<IConfigurationRoot>();
+
+            var source = new ConfigurationRootSource(rootMock.Object);
+
+            var result = source.Get(valueName, null, out string value);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Get_Returns_False_When_Exception_Is_Thrown()
+        {
+            const string valueName = "value";
+
+            var rootMock = new Mock<IConfigurationRoot>();
+            rootMock.Setup(x => x[It.IsAny<string>()])
+                .Throws<InvalidOperationException>();
+
+            var source = new ConfigurationRootSource(rootMock.Object);
+
+            var result = source.Get(valueName, null, out string value);
+
+            Assert.IsFalse(result);
+            rootMock.VerifyAll();
+        }
+
+        [TestMethod]
+        public void Get_Returns_True_When_Value_Exists()
+        {
+            const string valueName = "value";
+            const string expected = "something";
+
+            var rootMock = new Mock<IConfigurationRoot>();
+            rootMock.Setup(x => x[valueName])
+                .Returns(expected);
+
+            var source = new ConfigurationRootSource(rootMock.Object);
+
+            var result = source.Get(valueName, null, out string value);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(expected, value);
+            rootMock.VerifyAll();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Configgy.Microsoft.Extensions.Configuration
-Configgy extensions to incorporate Microsoft.Extensions.Configuration configuration mechanisms
+
+[Configgy](https://github.com/bungeemonkee/Configgy) extensions to incorporate [Microsoft.Extensions.Configuration](https://www.nuget.org/packages/Microsoft.Extensions.Configuration/) configuration mechanisms
+
+## Version Numbering
+
+Each version will have two numbers: a Windows file version and a SemVer version. The SemVer version will be derived from the Windows file version.
+
+Versioning will follow this pattern:
+
+* Major Version : Increased when there is a release that breaks a public api
+* Minor Version : Increased for each non-api-breaking release
+* Build Number : 0 for alpha versions, 1 for beta versions, 2 for release candidates, 3 for releases
+* Revision : Always 0 for release versions, always 1+ for alpha, beta, rc versions to indicate the alpha/beta/rc  number (alpha/beeta/rc numbers should always be two-digits)
+
+So the following versions are equalivalent
+
+* 1.0.0.1 => 1.0.0-alpha01
+* 1.0.1.1 => 1.0.1-beta01
+* 1.0.2.1 => 1.0.2-rc01
+* 1.0.3.0 => 1.0.3
